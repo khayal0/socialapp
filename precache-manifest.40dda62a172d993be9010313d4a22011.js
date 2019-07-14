3bc67dbe393fb8496fd7be6430662955efc7b145
xeyal_i <xeyal.isaxanov@gmail.com>
2019-07-14 16:17:06 +0400

    Updates

---
+++ precache-manifest.40dda62a172d993be9010313d4a22011.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6732bd426c60b47172b1c7296e886a60",
+    "url": "/socialapp/static/media/no-img.6732bd42.png"
+  },
+  {
+    "revision": "418b103590eee09a934c",
+    "url": "/socialapp/static/js/runtime~main.9655fe6e.js"
+  },
+  {
+    "revision": "35d7e7035d745ef25cb5",
+    "url": "/socialapp/static/js/main.65125f3e.chunk.js"
+  },
+  {
+    "revision": "610ba59de8084acd2146",
+    "url": "/socialapp/static/js/2.3842f050.chunk.js"
+  },
+  {
+    "revision": "35d7e7035d745ef25cb5",
+    "url": "/socialapp/static/css/main.2dcabae9.chunk.css"
+  },
+  {
+    "revision": "741a271df47a630f825a64683900fa9e",
+    "url": "/socialapp/index.html"
+  }
+];
